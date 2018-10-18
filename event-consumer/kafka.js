304744dfe9304993e8372bd436dfe89954212a7e
TonyTheCat <anton.shishenya@gmail.com>
2018-10-18 10:29:06 +0300

    Change stategis kafka

--- event-consumer/kafka.js
+++ event-consumer/kafka.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const kafka = require("no-kafka");
+const Promise = require("bluebird");
 
 /**
  * @param config - kafka config
@@ -8,46 +9,30 @@ const kafka = require("no-kafka");
  * @param errorHandler - error handler
  */
 function newConsumer(config, eventHandler, errorHandler) {
-    let consumer = new kafka.SimpleConsumer({
-        connectionString: config.kafka.hosts.join(","),
-        groupId: config["consumer-group"],
-        clientId: config.kafka["client-id"]
-    });
-    return consumer.init().then(() => {
-        return consumer.fetchOffset([{
-            topic: config.kafka.topic,
-            partition: 0
-        }])
-        .then(fetchOffset => {
-            let offset = fetchOffset[0].offset;
-            if (offset > -1) {
-                offset++;
-            }
-            consumer.subscribe(config.kafka.topic, 0, {offset}, (messageSet, topic, partition) => {
-                return Promise.all(messageSet.map(message =>
-                    eventHandler(JSON.parse(message.message.value.toString("utf8")))
-                ))
-                .then(() => {
-                    let last = messageSet[messageSet.length - 1];
-                    return consumer.commitOffset({
-                        topic,
-                        partition,
-                        offset: last.offset
-                    });
-                })
-                .catch(err => {
-                    errorHandler(err);
+    var dataHandler = (messageSet, topic, partition) => {
+        return Promise.all(messageSet.map(message =>
+            eventHandler(JSON.parse(message.message.value.toString("utf8")))
+        ))
+            .then(() => {
+                return Promise.each(messageSet, m => {
+                    return consumer.commitOffset({ topic: topic, partition: partition, offset: m.offset, metadata: "optional" });
                 });
+            })
+            .catch(err => {
+                errorHandler(err);
             });
-        })
-        .catch(err => {
-            errorHandler(err);
-            process.exit(101);
-        });
-    })
-    .catch(err => {
-        errorHandler(err);
-        throw err;
-    });
+    };
+    var consumer = new kafka.GroupConsumer(
+        {
+            connectionString: config.kafka.hosts.join(","),
+            groupId: config["consumer-group"],
+            clientId: config.kafka["client-id"]
+        }
+    );
+    var strategies = [{
+        subscriptions: [config.kafka.topic],
+        handler: dataHandler
+    }];
+    return consumer.init(strategies);
 }
 module.exports = newConsumer;
