a85a3a6c9d5bad524ecebc55428e737b4790f108
win-dox <schamraev.d@yandex.ru>
2019-05-24 18:53:18 +0300

    remake requests to client-data and file

--- file/index.js
+++ file/index.js
@@ -7,25 +7,48 @@ function newClient(url) {
         url,
         version: "*"
     });
+    const startTimeout = 5;
+    const deltaInterval = 20;
+    const maxTimeout = 3500;
+
+    const request = (method, url) => {
+        return new Promise((resolve, reject) => {
+            const callback = (err, req, res, obj) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(obj);
+            };
+            if (method === "GET") client.get(url, callback);
+        });
+    };
+
+    const timeout = async ms => {
+        return new Promise(res => setTimeout(res, ms));
+    };
+
+    const handle = async (method, url) => {
+        let err;
+        let waitTime = 0;
+        for (let i = startTimeout; waitTime <= maxTimeout; i += deltaInterval) {
+            try {
+                const res = await request(method, url);
+                return res;
+            } catch (e) {
+                err = e;
+                if (!e.message.includes("Gateway")) break;
+                await timeout(i);
+                waitTime += i;
+                continue;
+            }
+        }
+        throw new Error(err);
+    };
 
     return {
-        getImageUrl: file_id => new Promise((resolve, reject) => {
-            client.get(`/v1/internal/file/file_url?file_id=${file_id.fileId}`, (err, req, res, obj) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(obj);
-            });
-        }),
+        getImageUrl: file_id => handle("GET", `/v1/internal/file/file_url?file_id=${file_id.fileId}`),
 
-        internalUploadFile: params => new Promise((resolve, reject) => {
-            client.get(`/v1/internal/file/upload_file?mime_type=${params.params.mime_type}&title=${params.params.title}&kind=${params.params.kind}`, (err, req, res, obj) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(obj);
-            });
-        })
+        internalUploadFile: params => handle("GET", `/v1/internal/file/upload_file?mime_type=${params.params.mime_type}&title=${params.params.title}&kind=${params.params.kind}`)
     };
 }
 
