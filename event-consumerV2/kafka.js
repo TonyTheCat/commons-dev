7895cce9ff211585fc8cdf6de7a85ad797b6ff64
win-dox <schamraev.d@yandex.ru>
2019-07-16 12:13:18 +0300

    Add V2

---
+++ event-consumerV2/kafka.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const kafka = require("no-kafka");
+const Promise = require("bluebird");
+
+/**
+ * @param config - kafka config
+ * @param eventHandler - event handler
+ * @param errorHandler - error handler
+ */
+const newConsumer = (config, eventHandler, errorHandler) => {
+    const dataHandler = (messageSet, topic, partition) => {
+        return Promise.all(messageSet.map(message =>
+            eventHandler(JSON.parse(message.message.value.toString("utf8")))
+        ))
+            .then(() => {
+                return Promise.each(messageSet, m => {
+                    return consumer.commitOffset({ topic: topic, partition: partition, offset: m.offset, metadata: "optional" });
+                });
+            })
+            .catch(err => {
+                errorHandler(err);
+            });
+    };
+
+    function generateClientID() {
+        let text = "";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (let i = 0; i < 10; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+
+    let consumer = new kafka.GroupConsumer(
+        {
+            connectionString: config.kafka.hosts.join(","),
+            groupId: config["consumer-groupV2"],
+            clientId: generateClientID()
+        }
+    );
+    const strategies = [{
+        subscriptions: [config.kafka.topicV2],
+        handler: dataHandler
+    }];
+    return consumer.init(strategies);
+};
+
+module.exports = newConsumer;
